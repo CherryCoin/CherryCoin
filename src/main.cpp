9b659ea652abdeb5bbcc69b7112d245bef29c0cc
R3H4Bit <administrator@blckbinary.nl>
2014-03-07 21:03:05 +0100

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -2112,7 +2112,7 @@ bool CBlock::CheckBlock(CValidationState &state, bool fCheckPOW, bool fCheckMerk
     }
 
     // Check proof of work matches claimed amount
-    if (fCheckPOW && !CheckProofOfWork(GetPoWHash(), nBits)
+    if (fCheckPOW && !CheckProofOfWork(GetPoWHash(), nBits))
         return state.DoS(50, error("CheckBlock() : proof of work failed"));
 
     // Check timestamp
